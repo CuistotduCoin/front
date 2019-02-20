8b75ea50491a8974d60b44e9b06baa5be644fc55
Romain Quellec <romain.quellec@gmail.com>
2019-02-20 17:06:58 +0100

    serverless config

--- loadenv.js
+++ loadenv.js
@@ -4,7 +4,9 @@ const awsParamEnv = require('aws-param-env'); // eslint-disable-line
 awsParamEnv.load('/cuistot', { region: 'eu-west-1' });
 
 const content = `\
+AWS_DOMAIN=${process.env['domain']}
 AWS_REGION_IRELAND=eu-west-1
+AWS_DEPLOYMENT_BUCKET=${process.env['userpool-id']}
 AWS_USERPOOL_ID=${process.env['userpool-id']}
 AWS_IDENTITY_POOL_ID=${process.env['identity-pool-id']}
 AWS_STORE_BUCKET=${process.env['store-bucket']}
@@ -16,6 +18,7 @@ ALGOLIASEARCH_SEARCH_APP_ID=${process.env['algolia-search-app-id']}
 ALGOLIASEARCH_SEARCH_KEY=${process.env['algolia-search-key']}
 ALGOLIASEARCH_PLACES_APP_ID=${process.env['algolia-places-app-id']}
 ALGOLIASEARCH_PLACES_KEY=${process.env['algolia-places-key']}
+SENTRY_DSN=${process.env['sentry-dsn']}
 `;
 
 fs.writeFile('.env', content, (err) => {
