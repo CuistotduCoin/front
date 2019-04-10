885d3ee15ad238e13c8ced8b5d75b43f05628a55
Romain Quellec <romain.quellec@gmail.com>
2019-04-10 12:05:28 +0200

    modify bucket for cors

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const withMDX = require('@zeit/next-mdx')()
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 
 const nextConfig = {
-  assetPrefix: isProd ? 'https://s3.amazonaws.com/assets-cuistotducoin' : '',
+  assetPrefix: isProd ? 'https://s3-eu-west-1.com/assets-cuistotducoin' : '',
   target: 'serverless',
   distDir: 'build',
   generateBuildId: async () => {
