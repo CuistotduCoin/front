d047763958954cd6d61ac3d6e49da56826fe251c
Romain Quellec <romain.quellec@gmail.com>
2019-05-06 17:08:33 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,4 +1,6 @@
 const compat = require("serverless-nextjs-plugin/aws-lambda-compat");
+const Raven = require("raven");
+const RavenLambdaWrapper = require("serverless-sentry-lib")
 
 module.exports = page => {
     return RavenLambdaWrapper.handler(Raven, (event, context, callback) => {
