8c0ffc880c5eef1dd6cb7669893ea5f3d50d2438
Romain Quellec <romain.quellec@gmail.com>
2019-09-02 15:00:31 +0200

    update yarn

--- shared/date-utils.ts
+++ shared/date-utils.ts
@@ -1,9 +1,13 @@
-import dateFnsDistanceInWordsToNow from 'date-fns/distance_in_words_to_now';
-import dateFnsFormat from 'date-fns/format';
-import fr from 'date-fns/locale/fr';
+import {
+    format as dateFnsFormat,
+    formatDistanceToNow as dateFnsDistanceInWordsToNow
+} from "date-fns";
+import { fr } from "date-fns/locale";
 
 const locales = { fr };
 const locale = { locale: locales.fr }; // use locales[window.__localeId__] or locales[global.__localeId__] when multi language
 
-export const format = (date, formatStr) => dateFnsFormat(date, formatStr, locale);
-export const distanceInWordsToNow = (date) => dateFnsDistanceInWordsToNow(date, locale);
+export const format = (date, formatStr) =>
+    dateFnsFormat(date, formatStr, locale);
+export const distanceInWordsToNow = date =>
+    dateFnsDistanceInWordsToNow(date, locale);
