8dc9a86fa7b431ddf9956cf112f27809dca347e9
Romain Quellec <romain.quellec@gmail.com>
2019-12-04 16:44:35 +0100

    Update individual.tsx

--- pages/individual.tsx
+++ pages/individual.tsx
@@ -250,6 +250,201 @@ class Individual extends React.Component<IIndividualProps, IIndividualState> {
         typeform: "https://cuistotducoin.typeform.com/to/oVj49e",
         type: "Atelier collectif"
       }
+      {
+        availableSeat: 8,
+        date: "Vendredi 3 Janvier, 14h00-16h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/anne-chocolat-detente.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/anne.jpg",
+        name: "Parents/Enfants - Gâteau sans gluten",
+        nameCook: "Anne",
+        price: 40,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/D9vG9Y",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Samedi 4 Janvier, 14h00-16h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/anne-barre.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/anne.jpg",
+        name: "Enfants - Barres Chocolatées",
+        nameCook: "Anne",
+        price: 30,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/j8lh6d",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Samedi 11 Janvier, 14h00-16h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/audrey-galette-des-rois.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/audrey.jpg",
+        name: "Enfants - Galette des Rois",
+        nameCook: "Audrey",
+        price: 30,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/rPoBIS",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Vendredi 17 Janvier, 17h30-20h30",
+        duration: 3,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/luis-gnocchi.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/luis.jpg",
+        name: "Atelier Italien - Gnocchis",
+        nameCook: "Luis",
+        price: 50,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/ARhx5N",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Samedi 18 Janvier, 10h00-12h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/audrey-galette-des-rois.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/audrey.jpg",
+        name: "Adultes - Galette des Rois",
+        nameCook: "Audrey",
+        price: 35,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/ldY9Fp",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Samedi 18 Janvier, 14h00-16h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/anne-mousse-au-chocolat.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/anne.jpg",
+        name: "Atelier Healthy - 100% Desserts",
+        nameCook: "Anne",
+        price: 35,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/xpxQQM",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Samedi 25 Janvier, 10h00-12h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/farah-mezze.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/farah.jpg",
+        name: "Atelier Libanais - Mezzé",
+        nameCook: "Farah",
+        price: 35,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/dXZxzX",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Samedi 1er Février, 10h00-12h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/anne-granola.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/anne.jpg",
+        name: "Parents/Enfants - Petit Dej' Energie",
+        nameCook: "Anne",
+        price: 40,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/NEEsQV",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Samedi 1er Février, 14h00-16h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/anne-bien-etre.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/anne.jpg",
+        name: "Atelier Healthy - Apéro Oméga et Vitamines",
+        nameCook: "Anne",
+        price: 35,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/EW38IZ",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Samedi 22 Février, 10h00-12h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/farah-mezze.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/farah.jpg",
+        name: "Atelier Libanais - Mezzé",
+        nameCook: "Farah",
+        price: 35,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/f9xSFd",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Mercredi 26 Février, 14h00-16h00",
+        duration: 2,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/anne-bien-etre.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/anne.jpg",
+        name: "Enfants - Apéro en Couleur",
+        nameCook: "Anne",
+        price: 30,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/ZOkOq5",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Vendredi 27 Mars, 17h30-20h30",
+        duration: 3,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/luis-cannelloni.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/luis.jpg",
+        name: "Atelier Italien - Cannelloni",
+        nameCook: "Luis",
+        price: 50,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/LlpZc0",
+        type: "Atelier collectif"
+      },
+      {
+        availableSeat: 8,
+        date: "Vendredi 10 Avril, 17h30-20h30",
+        duration: 3,
+        image:
+          "https://static.cuistotducoin.com/img/workshops/luis-agnolotti.jpg",
+        imageCook: "https://static.cuistotducoin.com/img/cooks/luis.jpg",
+        name: "Atelier Italien - Agnolotti",
+        nameCook: "Luis",
+        price: 50,
+        spot: "Cuisinella, Brest",
+        totalSeat: 8,
+        typeform: "https://cuistotducoin.typeform.com/to/ARhx5N",
+        type: "Atelier collectif"
+      }
     ];
 
     const privateWorkshops = [
